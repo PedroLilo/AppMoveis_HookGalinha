30aee3b852599225ecad2d652b0d0ea7956320c3
PedroLilo <pedrohenriquelilo557@gmail.com>
2025-06-18 08:50:07 -0300

    pronto

--- src/components/Botão/BotaoInterativo.tsx
+++ src/components/Botão/BotaoInterativo.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import {useState} from 'react';
-import { StyleSheet, View, Text, TouchableOpacity, Image } from 'react-native';
+import {useState, useEffect} from 'react';
+import { StyleSheet, View, Text, Pressable, Image, Alert } from 'react-native';
 import { styles } from './BotaoInterativoStyle';
 
 const galinha = require('../../../assets/GalinhaNormal.png');
@@ -10,6 +10,8 @@ const galinhaVermelha = require('../../../assets/GalinhaApanhando.png');
 export function BotaoGalinha() {
     const [qtd, setQtd] = useState(0);
     const [img, setImg] = useState(galinha);
+    const [tempo, setTempo] = useState(0);
+    const [media, setMedia] = useState(0);
 
 
     function aumentar() {
@@ -19,21 +21,61 @@ export function BotaoGalinha() {
     
     function pin() {
         setImg(galinhaVermelha)
+        console.log(qtd)
+        if(qtd == 0){
+            setTempo(0)
+        }
     }
+    
 
     function pout(){
         setImg(galinha)
     }
 
+    function plongo(){
+        mediaTempo()
+        Alert.alert("Média de cliques por segundo", ''+media)
+    }
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setTempo((prevCount) => prevCount + 1);
+        }, 1000);
+    
+        return () => clearInterval(timer);
+      }, [tempo]);
+
+    function mediaTempo() {
+        console.log(qtd)
+        console.log(tempo)
+        setMedia(qtd / tempo)
+      }
+    
+    function zerar() {
+        setMedia(0)
+        setQtd(0)
+        setTempo(0)    
+    }
+
     return (
         <View style={styles.container}>
-            <TouchableOpacity onPress={(aumentar)}
+            <Pressable onPress={(aumentar)}
                               onPressIn={pin}
                               onPressOut={pout}
+                              onLongPress={plongo}
             >
-                <Image source={img}></Image>
-            </TouchableOpacity>
-            <Text>Total de Cliques: {qtd}</Text>
+                <Image style={styles.galinhaimagem} source={img}></Image>
+            </Pressable>
+            
+            <View style={styles.viewStats}>
+                <Text  style={styles.stats}>Total de Cliques: {qtd}</Text>
+                <Text  style={styles.stats}>Tempo Total: {tempo}</Text>
+                <Text  style={styles.stats}>Velocidade de Cliques: {media}</Text>
+            </View>
+
+            <Pressable style={styles.zerar} onPress={(zerar)}>
+                <Text style={styles.texxtozerar}>ZERAR</Text>
+            </Pressable>
         </View>
     
   );
--- src/components/Botão/BotaoInterativoStyle.tsx
+++ src/components/Botão/BotaoInterativoStyle.tsx
@@ -1,6 +1,37 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 export const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  zerar:{
+    backgroundColor: '#ff0000',
+    width: 200,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 50,
+    borderStyle: 'solid',
+    borderWidth: 2,
+    borderRadius: 30,
+    borderColor: "black" 
+  },
+  texxtozerar:{
+    fontSize: 20,
+    color: '#fff',
+  },
+  galinhaimagem:{
+    width: 240,
+    height: 290,
+    marginBottom: 50,
+  },
+  stats:{
+    fontSize: 20,
+  },
+  viewStats:{
+
   }
 });
