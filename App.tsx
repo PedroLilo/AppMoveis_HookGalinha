e9c0876fbbc76684ca41f9def56a5a7b574e7511
Pedro Liló <160170134+PedroLilo@users.noreply.github.com>
2025-06-18 08:57:33 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,3 +1,5 @@
+/*Grupo: Pedro Henrique Liló, Julia Garcia Braga dos Reis*/
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Home from './src/screens/home/Home';
